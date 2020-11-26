dcbbc5d36040c30e1b59094d2ee4ad87fbba69a8
kola1197 <kola-1197@mail.ru>
2020-11-26 14:03:49 +0300

    new icon

--- DebugServer/Network/main.cpp
+++ DebugServer/Network/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[]) {
     MainWindow w;
     w.setWindowTitle("Simulation");
     QIcon icon;
-    icon.addFile(QStringLiteral("../icon.ico"), QSize(), QIcon::Normal, QIcon::Off);
+    icon.addFile(QStringLiteral("../icon1.ico"), QSize(), QIcon::Normal, QIcon::Off);
     w.setWindowIcon(icon);
     w.setWindowIconText("Simulation");
     w.show();
